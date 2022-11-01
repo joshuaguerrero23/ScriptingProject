7b60a9fcb6d5dc88c513f8f0baaff34430203c25
joshuaguerrero23 <joshtheframe@gmail.com>
2022-11-01 14:39:02 -0700

    Added Scene
    Added a scence

---
+++ ScriptingProjectGuerreroP7/Assets/Scripts/Loops.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+    int numEnemies = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+
+        
+    }
+}
